59539a1cf3e3a84f791dab9e0dbd71aac5777e2c
d7o1d1s0 <d7sarmy@gmail.com>
2021-10-25 16:22:28 +0100

    changed some text

--- frontend/src/containers/InfoContainer.js
+++ frontend/src/containers/InfoContainer.js
@@ -7,7 +7,7 @@ const InfoContainer = ({ }) => {
 
     return (<>
         <h2>Welcome to Raquepesos.money</h2>
-        <p>The coins here are all for fun - buy some raquepesos on uniswap and stake them here to watch them grow!</p>
+        <p>The coins here are all for fun - buy some raquepesos on uniswap to get started. Then connect your wallet and stake them here to watch them grow!</p>
         {/* <FarmContainer/> */}
     </>
     )
